7dd0d8a030084c5f565fc3bbd18d095f3622bddf
Concentino <concentino0@gmail.com>
2017-11-06 19:53:36 -0200

    Atualizacao ex 5

--- lista03/5.c
+++ lista03/5.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+
+int cmpfunc (const void * a, const void *b) {
+    return (*(int*)a - *(int*)b );
+
+}
 //Preenche vetor
 void completa (int *v, int TAM)
 {
@@ -13,27 +18,17 @@ void completa (int *v, int TAM)
 //Verifica se há repetição de números dentro do vetor
 void verifica (int *v, int TAM)
 {
-    int aux [10], i, j;
-
-    for (i=0; i<10; i++) {
-        aux [i] =0;
-    }
+    int aux [TAM], i, j, NUM;
 
     for (i=0; i<TAM; i++) {
-        if (v[i]==0) aux [0]++;
-        if (v[i]==1) aux [1]++;
-        if (v[i]==2) aux [2]++;
-        if (v[i]==3) aux [3]++;
-        if (v[i]==4) aux [4]++;
-        if (v[i]==5) aux [5]++;
-        if (v[i]==6) aux [6]++;
-        if (v[i]==7) aux [7]++;
-        if (v[i]==8) aux [8]++;
-        if (v[i]==9) aux [9]++;
+            NUM = 0;
+        for (j=i; j<TAM; j++) {
+            if (v[i]==v[j]) NUM++;
+        }
+        if (NUM>1) {j=TAM; printf("%i-%i ", v[i], NUM);}
+        while (v[i]==v[i+1]) i++;
     }
-    for (i=0; i<10; i++) {
-        if (aux [i]!=0) printf("%i-%i\n", i, aux [i]);
-    }
+
 }
 
 int main()
@@ -43,17 +38,15 @@ int main()
     scanf("%i", &TAM);
 
     int vetor [TAM];
-    
+
 	/*int *vetor;
 
     vetor = malloc(sizeof(int)*TAM);*/
 
-    
-    completa (vetor, TAM);
-    
 
+    completa (vetor, TAM);
+    qsort(vetor, TAM, sizeof(int), cmpfunc);
     verifica (vetor, TAM);
-	
 	printf ("\n");
 
     return 0;
